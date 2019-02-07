c3d5bcdaa91eb27c10141b93b6d5ee09a56f6a13
Юрий  Привалов <y.privalov@talmer.ru>
2019-02-07 20:01:20 +0300

    added description incident

--- servicedesc-create-incident.php
+++ servicedesc-create-incident.php
@@ -1,5 +1,7 @@
 <?php
 
+//ini_set('arg_separator.output','&amp;');
+
 // Задаем URL, по которому будем обращаться к REST API:
 $url='http://192.168.43.250/sdpapi/request';
 
@@ -7,28 +9,37 @@ $url='http://192.168.43.250/sdpapi/request';
 $state=$argv[1];
 
 // Принимаем имя хоста от icinga в качестве второго параметра скрипта:
-$subcategory=$argv[2];
+$host=$argv[2];
 
 // Принимаем имя сервиса от icinga в качестве третьего параметра скрипта:
-$item=$argv[3];
+$service=$argv[3];
 
 // Формируем тему инцидента:
-$subject=$state." state on ".$subcategory." with service ".$item;
+//$subject=$state." state on ".$host." with service ".$service;
+$subject=$state." service ".$service." on ".$host;
+
+// Формируем описание инцидента
+$desc='Host: '.$host.' <br/> Service: '.$service.' <br/> Status: '.$state.' <br/><br/> http://192.168.42.58/icingaweb2/monitoring/service/show?host='.$host.'%26service='.$service.'';
+
+// echo $desc;
 
 // Задаем API Key специалиста:
 $TK='50B971C0-4C98-4011-995E-30B17B957CCD';
 
 // Формируем инцидент:
-$request_string=<<<JSON
+$request_string='
 {
     "operation": {
         "details": {
             "requester": "Monitoring",
             "requestType": "Incident",
-            "subject": "$subject",
+            "subject": "' . $subject . '",
             "category": "Monitoring",
-            "subcategory": "$subcategory",
-            "description": "Specify Description",
+            "Хост": "' . $host . '",
+            "Служба": "' . $service . '",
+            "Статус": "' . $state . '",
+            "requesttemplate": "Monitoring",
+            "description": "' . $desc . '",
             "priority": "High",
             "site": "New York",
             "group": "Инфраструктура",
@@ -37,7 +48,7 @@ $request_string=<<<JSON
         }
     }
 }
-JSON;
+';
 
 // Инициализируем curl-сессию:
 $ch = curl_init();
@@ -51,12 +62,18 @@ curl_setopt($ch, CURLOPT_POST, true);
 /* Передаем строку с данными для передачи HTTP POST.
 Для REST API параметром OPERATION_NAME=ADD_REQUEST даем команду на добавление инцидента, параметром INPUT_DATA
 передаем XML-документ, а в конце строки указываем API Key в параметре TECHNICIAN_KEY */
-curl_setopt($ch, CURLOPT_POSTFIELDS, "OPERATION_NAME=ADD_REQUEST&INPUT_DATA=$request_xmlstring&TECHNICIAN_KEY=$TK");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "OPERATION_NAME=ADD_REQUEST&INPUT_DATA=$request_string&TECHNICIAN_KEY=$TK&format=json");
 
 // Открываем curl-сессию:
 curl_exec($ch);
 
 // Закрываем curl-сессию:
 curl_close($ch);
+
+// DEBUG
+//$server_output = curl_exec ($ch);
+//$error_message = curl_errno($ch);
+//curl_close ($ch);
+//print_r($server_output);
 
 ?>
